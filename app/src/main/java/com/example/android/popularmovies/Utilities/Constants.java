0ba91ca5fb8c32371193abcd031e3b7fc996b83f
D'Narial Brown-Pressley <40445074+darkcain1993@users.noreply.github.com>
2018-12-30 21:31:54 -0800

    Update Constants.java

--- app/src/main/java/com/example/android/popularmovies/Utilities/Constants.java
+++ app/src/main/java/com/example/android/popularmovies/Utilities/Constants.java
@@ -5,24 +5,14 @@ import com.example.android.popularmovies.BuildConfig;
 // this class is used to hold constants that may be used in multiple places.
 public abstract class Constants {
 
-    private static final String apiKey = BuildConfig.APIKey;
-<<<<<<< HEAD
+  private static final String apiKey = BuildConfig.APIKey;
     public static final String popularSortLink = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey ;
     public static final String topRatingSortLink = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKey;
     public static final String tLink1 = "https://api.themoviedb.org/3/movie/";
     public static final String tLink2 = "/videos?api_key=" + apiKey;
     public static final String rLink1 = "https://api.themoviedb.org/3/movie/";
     public static final String rLink2 = "/reviews?api_key=" + apiKey;
-=======
-    public static final String popularSortLink = "https://api.themoviedb.org/3/movie/popular?api_key=" + "Enter Key Here";
-    public static final String topRatingSortLink = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + "Enter Key Here";
-    public static final String tLink1 = "https://api.themoviedb.org/3/movie/";
-    public static final String tLink2 = "/videos?api_key=" + "Enter Key Here";
-    public static final String rLink1 = "https://api.themoviedb.org/3/movie/";
-    public static final String rLink2 = "/reviews?api_key=" + "Enter Key Here";
->>>>>>> 5629cd526d3f017ce5ddeb4b388c89aa3432ccef
     public static final String imageUrl1 = "http://image.tmdb.org/t/p/" + "w780";
     public static final String imageUrl = "http://image.tmdb.org/t/p/" + "w342" ;
     public static final String trailerVideo = "https://www.youtube.com/watch?v=";
-
 }
